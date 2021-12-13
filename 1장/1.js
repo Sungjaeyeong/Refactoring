d1529d898204db311974cf58d41586e2a260a0e3
성재영 <jaeyeong@MacBookPro.local>
2021-12-13 22:57:23 +0900

    PerformanceCalculator 클래스 생성

--- 1장/1.js
+++ 1장/1.js
@@ -28,8 +28,40 @@ function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
+}
+
 function htmlStatement(invoice, plays) {
   return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>`;
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += `</table>\n`;
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 function createStatementData(invoice, plays) {
@@ -41,6 +73,7 @@ function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance); // 공연료 계산기 생성
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = playFor(result);
     result.amount = amountFor(result);
@@ -96,34 +129,8 @@ function createStatementData(invoice, plays) {
   }
 }
 
-function renderPlainText(data) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-  for (let perf of data.performances) {
-    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+class PerformanceCalculator {
+  constructor(aPerformance) {
+    this.performance = aPerformance;
   }
-  result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-  return result;
-}
-
-function renderHtml(data) {
-  let result = "<h1>청구 내역 (고객명: ${data.customer})</h1>\n";
-  result += "<table>\n";
-  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
-  for (let perf of data.performances) {
-    result += " <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>";
-    result += "<td>${usd(perf.amount)}</td></tr>\n";
-  }
-  result += "</table>\n";
-  result += "<p>총액: <em>${usd(data.totalAmount)}</em></p>\n";
-  result += "<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n";
-  return result;
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
 }
