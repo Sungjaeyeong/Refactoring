58589c74746db5dbf3bcbc8b22639e7ad1b4ffd0
성재영 <jaeyeong@MacBookPro.local>
2021-12-13 23:24:36 +0900

    다형성 구조 갖추기

--- 1장/1.js
+++ 1장/1.js
@@ -99,6 +99,16 @@ function createStatementData(invoice, plays) {
   }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error("알 수 없는 장르: ${aPlay.type}");
+  }
+}
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
@@ -136,6 +146,6 @@ class PerformanceCalculator {
   }
 }
 
-function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
-}
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
