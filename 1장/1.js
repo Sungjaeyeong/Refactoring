c9793f4b868f5b213c754ca76880b489da860d5d
성재영 <jaeyeong@MacBookPro.local>
2021-12-13 23:31:09 +0900

    1장 완료

--- 1장/1.js
+++ 1장/1.js
@@ -109,6 +109,7 @@ function createPerformanceCalculator(aPerformance, aPlay) {
       throw new Error("알 수 없는 장르: ${aPlay.type}");
   }
 }
+
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
