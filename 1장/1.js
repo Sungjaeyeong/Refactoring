6282c247005ec82869ea1d693c95988da14cf073
성재영 <jaeyeong@MacBookPro.local>
2021-12-13 20:30:12 +0900

    totalAmount 변수 제거

--- 1장/1.js
+++ 1장/1.js
@@ -25,7 +25,6 @@ const invoice = [
 ];
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   function usd(aNumber) {
@@ -81,15 +80,22 @@ function statement(invoice, plays) {
     return volumeCredits;
   }
 
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
