7b55899d78b6b978707f0ebc2a4118c867fa6930
성재영 <jaeyeong@MacBookPro.local>
2021-12-13 19:13:54 +0900

    amoutFor 함수추출

--- 1장/1.js
+++ 1장/1.js
@@ -27,15 +27,16 @@ const invoice = [
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = "청구 내역 (고객명: ${invoice.customer})\n";
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+
+  function amountFor(perf, play) {
     let thisAmount = 0;
+
     switch (play.type) {
       case "tragedy": // 비극
         thisAmount = 40000;
@@ -53,15 +54,25 @@ function statement(invoice, plays) {
       default:
         throw new Error("알 수 없는 장르: ${play.type}");
     }
+    return thisAmount;
+  }
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
+
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
     // 청구 내역을 출력한다.
-    result += " ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n";
+    result += `${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
     totalAmount += thisAmount;
   }
-  result += "총액: ${format(totalAmount/100)}\n";
-  result += "적립 포인트: ${volumeCredits}점\n";
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
