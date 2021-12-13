948e47a8c4467032c58e184de54b44076321622f
성재영 <jaeyeong@MacBookPro.local>
2021-12-13 22:28:48 +0900

    총합 구하는부분 옮기기

--- 1장/1.js
+++ 1장/1.js
@@ -28,6 +28,8 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -74,24 +76,6 @@ function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
-}
-
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-  for (let perf of data.performances) {
-    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
 
   function totalVolumeCredits() {
     let result = 0;
@@ -107,5 +91,23 @@ function renderPlainText(data, plays) {
       result += perf.amount;
     }
     return result;
+  }
+}
+
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
   }
 }
